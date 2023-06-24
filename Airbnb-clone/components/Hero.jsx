67dda0bf5f7f1bebc3e5a3431ad9045fdb9ff142
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-24 10:42:26 +0300

    creating the hero section

---
+++ Airbnb-clone/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <>
+            <img src="../src/assets/photo-grid.png" alt="" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activitites led by one-of-a-kind hosts--all without leaving home.</p>
+        </>
+    )
+}
+
+export default Hero
